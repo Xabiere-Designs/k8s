cad001134969b9182009b1deeb16219f67036993
xabiere-designs <xabieredesigns@gmail.com>
2024-05-05 23:30:18 -0400

    update dotnet c#

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace csharpfibb
+{
+
+    class Program
+    {
+
+        public static int fib(int i)
+        {
+
+            if (i <= 2) return 1;
+            return fib(i - 2) + fib(i - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(fib(20));
+        }
+    }
+}
